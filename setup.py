1cf81bb95003adc09cfc447ab855eda93beef9de
Tomas Chvatal <tomas.chvatal@gmail.com>
2015-07-08 17:30:36 +0200

    Add new setup.py file for easy deployment

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Setup file for easy installation."""
+from setuptools import setup
+import glob
+from openmoo2 import __version__
+
+setup(
+    name='openmoo2',
+    description='Master of Orion II',
+    long_description='open source clone of Microprose game "Master of Orion II: Battle at Antares"',
+    url='http://openmoo2.org/',
+    download_url='https://github.com/scarabeusiv/openmoo2',
+
+    version=__version__,
+
+    author='Petr Mika',
+    author_email='peterman@email.cz',
+
+    maintainer='Tomáš Chvátal',
+    maintainer_email='tomas.chvatal@gmail.com',
+
+    license='License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+    platforms=['Linux'],
+    keywords=['Strategy', 'Game'],
+
+    packages=['openmoo2'],
+
+    data_files=[('/usr/share/openmoo2/', glob.glob('data/*')),
+    ],
+
+    entry_points={
+        'console_scripts': ['openmoo2 = openmoo2:main'],
+        'console_scripts': ['openmoo2-server = openmoo2:server']
+    },
+)
+
