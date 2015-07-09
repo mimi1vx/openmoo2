c42d752199950d2e28edee95481f8f76bf5e1510
Tomas Chvatal <tomas.chvatal@gmail.com>
2015-07-09 16:53:58 +0200

    Split out the old stuff, too mesy and start from scratch

--- openmoo2/orion.py
+++ openmoo2/orion.py
@@ -1,17 +1,5 @@
 # vim: set ts=4 sw=4 et: coding=UTF-8
 
-import sys
-import pygame
-
-import cli
-import networking
-import game
-import rules
-import dictionary
-import autoplayer
-import gui
-
-
 class Orion(object):
     """
     Main object creating the game up to our likings
@@ -27,40 +15,15 @@ class Orion(object):
         """
         Run all the operations required to initialize server
         """
-    GAME = game.Game(rules.DEFAULT_RULES)
-    GAME.load_moo2_savegame(self.moo2_dir + "/" + self.options['savegame'])
-#   GAME.show_stars()
-#   GAME.show_planets()
-    GAME.show_players()
-#   GAME.show_colonies()
-    GAME.show_ships()
-
-    SERVER = networking.GameServer(self.options['hostname'], self.options['port'], GAME)
-    SERVER.set_name(GAME_FILE.split("/")[-1])
-    SERVER.run()
+        return
 
     def start_client(self):
         """
         Run all operations to start the client
         """
-        SOCKET_BUFFER_SIZE = 4096
-        gui.GUI.init(self.moo2_dir)
-        pygame.mouse.set_visible(False)
-        gui.splash_screen.Screen.draw()
-        gui.GUI.flip()
-        networking.Client.connect(self.options['hostname'], self.options['port'], SOCKET_BUFFER_SIZE)
-        networking.Client.login(self.options['user'])
-        WINDOW_CAPTION  = "OpenMOO2: PLAYER_ID = %s" % PLAYER_ID
-        pygame.display.set_caption(WINDOW_CAPTION)
-        ICON = pygame.image.load(self.moo2_dir + "/orion2-icon.png")
-        pygame.display.set_icon(ICON)
-        gui.GUI.run()
-        networking.Client.disconnect()
+        return
 
     def run(self):
-        if option.server:
-            self.start_server()
-        else:
+        self.start_server()
+        if not option.server:
             self.start_client()
-
-MOO2Start
--- openmoo2/oriondataloader.py
+++ openmoo2/oriondataloader.py
@@ -6,38 +6,35 @@ class OrionDataLoader(Object):
     """
     Class providing interface to load various datafiles
     """
-    datadir = None
 
-def __init__(self):
-    self.find_datadir()
+    def __init__(self):
+        self.datadir = self.find_datadir()
 
 
-def find_datadir(self):
-    """
-    Determine datadir we want to work with
-    """
-    datadirs = [
-        '{0}/../data'.format(os.path.dirname(os.path.realpath(__file__))),
-        '/usr/share/openmoo2',
-    ]
-    for datadir in datadirs:
-        if os.path.isdir(datadir):
-            self.datadir = datadir
-            break
+    def find_datadir(self):
+        """
+        Determine datadir we want to work with
+        """
+        datadirs = [
+            '{0}/../data'.format(os.path.dirname(os.path.realpath(__file__))),
+            '/usr/share/openmoo2',
+        ]
+        for datadir in datadirs:
+            if os.path.isdir(datadir):
+                return datadir
 
-    if not self.datadir:
         raise OrionException('Failed to find datadir, something is fishy')
 
-def provide_lbx_datadir(self):
-    """
-    Provide path for lbx datafiles
-    """
+    def provide_lbx_datadir(self):
+        """
+        Provide path for lbx datafiles
+        """
 
-    return '{0}/lbx/'.format(self.datadir)
+        return '{0}/lbx/'.format(self.datadir)
 
-def provide_datadir(self):
-   """
-   Provide path to datadir itself
-   """
+    def provide_datadir(self):
+        """
+        Provide path to datadir itself
+        """
 
-   return '{0}/'.format(self.datadir)
+        return '{0}/'.format(self.datadir)
