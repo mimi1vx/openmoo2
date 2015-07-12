f18fc7c65666eb3d77c0932b7aa0850391cb981b
Ondřej Súkup <mimi.vx@gmail.com>
2015-07-12 23:09:08 +0200

    make pep8 happier

--- tests/test_planet.py
+++ tests/test_planet.py
@@ -11,23 +11,23 @@ class Test_Planet(object):
     @classmethod
     def setup_class(cls):
         cls.random_planet = planet.Planet()
-        cls.home_planet = planet.Planet(user_planet = True)
+        cls.home_planet = planet.Planet(user_planet=True)
 
     def test_planet_setup(self):
-        ok_(self.random_planet.setup != None, "random planet has setup 'None'")
+        ok_(self.random_planet.setup is not None, "random planet has setup 'None'")
         ok_(self.home_planet.setup == "Normal", "home planet has another setup than 'Normal'")
 
     def test_planet_size(self):
-        ok_(self.random_planet.size != None, "random planet has size 'None'")
+        ok_(self.random_planet.size is not None, "random planet has size 'None'")
         ok_(self.home_planet.size == "Medium", "home planet has another size than 'Medium'")
 
     def test_planet_biology(self):
-        ok_(self.random_planet.biology != None, "random planet has biology 'None'")
+        ok_(self.random_planet.biology is not None, "random planet has biology 'None'")
         ok_(self.home_planet.biology == "Terran", "home planet has another biology than 'Terran'")
 
     def test_planet_minerals(self):
-        ok_(self.random_planet.minerals != None, "random planet has minerals 'None'")
-        ok_(self.home_planet.minerals == "Abundant" , "home planet has another minerals than 'Abundant'")
+        ok_(self.random_planet.minerals is not None, "random planet has minerals 'None'")
+        ok_(self.home_planet.minerals == "Abundant", "home planet has another minerals than 'Abundant'")
 
     def test_planet_gravity(self):
         """TODO : add with random generator for gravity"""
